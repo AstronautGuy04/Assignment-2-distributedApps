ee8f1ba4110bbaea2094b6874b3896011881eb1a
AstronautGuy04 <pu040200@gmail.com>
2024-11-28 20:22:46 -0500

    updated add.js

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,19 +1,8 @@
 const express = require('express')
 const app = express()
 
-const greetings = [
-    { timeOfDay: 'Morning', language: 'English', greetingMessage: 'Good morning', tone: 'Casual' },
-    { timeOfDay: 'Morning', language: 'English', greetingMessage: 'I wish you a pleasant morning', tone: 'Formal' }
-]
-
-app.use(express.json())
-
 app.get('/', (req, res) => {
-    res.json({ message: 'API Working!' })
-})
-
-app.get('/test', (req, res) => {
-    res.json({ greetings })
+    res.json({ message: 'Hello World!' })
 })
 
 module.exports = app
